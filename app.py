518531bdaae4efc9f12354e2982880f8534ecbc5
Devi Chandran .S <devuz19693@gmail.com>
2026-02-14 03:03:14 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+with open("index.html", "r", encoding="utf-8") as f:
+    html_code = f.read()
+
+st.components.v1.html(html_code, height=800, scrolling=True)
